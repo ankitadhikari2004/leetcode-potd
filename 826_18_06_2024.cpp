cadc5e0b779c62dbafcbe55a9c40af0aa4ef6303
Ankit  Adhikari <ankitadhikari@Ankits-MacBook-Air.local>
2024-06-18 23:50:02 +0530

    second push

--- 826_18_06_2024.cpp
+++ 826_18_06_2024.cpp
@@ -40,3 +40,54 @@ public:
 
 
 // approach 2
+
+class Solution
+{
+public:
+    int maxProfitAssignment(vector<int> &difficulty, vector<int> &profit, vector<int> &worker)
+    {
+
+        vector<pair<int, int>> v;
+        for (int i = 0; i < difficulty.size(); i++)
+        {
+            int diff = difficulty[i];
+            int pro = profit[i];
+            v.push_back({diff, pro});
+        }
+
+        sort(v.begin(), v.end());
+
+        int totalProfit = 0;
+
+        for (int i = 1; i < v.size(); i++)
+        {
+            v[i].second = max(v[i].second, v[i - 1].second);
+        }
+
+        for (int i = 0; i < worker.size(); i++)
+        {
+            int workerDiff = worker[i];
+            int low = 0;
+            int high = v.size() - 1;
+            int maxProfit = 0;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (v[mid].first <= workerDiff)
+                {
+                    maxProfit = max(maxProfit, v[mid].second);
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+            totalProfit += maxProfit;
+        }
+
+        return totalProfit;
+    }
+};
